97b4f33228c22dc18ca66c4ad3205ebb893f43ce
Nicolás <nmayorga092@gmail.com>
2023-01-20 16:21:42 -0300

    Sync

--- build.sh
+++ build.sh
@@ -6,19 +6,25 @@ source utils.sh
 trap "rm -rf temp/tmp.*" INT
 
 : >build.md
-mkdir -p "$BUILD_DIR" "$TEMP_DIR"
 
 toml_prep "$(cat 2>/dev/null "${1:-config.toml}")" || abort "could not find config file '${1}'"
 main_config_t=$(toml_get_table "")
 COMPRESSION_LEVEL=$(toml_get "$main_config_t" compression-level) || abort "ERROR: compression-level is missing"
 ENABLE_MAGISK_UPDATE=$(toml_get "$main_config_t" enable-magisk-update) || abort "ERROR: enable-magisk-update is missing"
 PARALLEL_JOBS=$(toml_get "$main_config_t" parallel-jobs) || abort "ERROR: parallel-jobs is missing"
-UPDATE_PREBUILTS=$(toml_get "$main_config_t" update-prebuilts) || abort "ERROR: update-prebuilts is missing"
 BUILD_MINDETACH_MODULE=$(toml_get "$main_config_t" build-mindetach-module) || abort "ERROR: build-mindetach-module is missing"
 LOGGING_F=$(toml_get "$main_config_t" logging-to-file) || LOGGING_F=false
 
+PATCHES_SRC=$(toml_get "$main_config_t" patches-source) || PATCHES_SRC="revanced/revanced-patches"
+INTEGRATIONS_SRC=$(toml_get "$main_config_t" integrations-source) || INTEGRATIONS_SRC="revanced/revanced-integrations"
+RV_BRAND=$(toml_get "$main_config_t" rv-brand) || RV_BRAND="ReVanced"
+RV_BRAND_F=${RV_BRAND,,}
+RV_BRAND_F=${RV_BRAND_F// /-}
+PREBUILTS_DIR="${TEMP_DIR}/tools-${RV_BRAND_F}"
+mkdir -p "$BUILD_DIR" "$PREBUILTS_DIR"
+
 if ((COMPRESSION_LEVEL > 9)) || ((COMPRESSION_LEVEL < 0)); then abort "compression-level must be from 0 to 9"; fi
-if [ "$UPDATE_PREBUILTS" = true ]; then get_prebuilts; else set_prebuilts; fi
+if [ "${DRYRUN:-}" = true ]; then set_prebuilts; else get_prebuilts || set_prebuilts; fi
 if [ "$BUILD_MINDETACH_MODULE" = true ]; then : >$PKGS_LIST; fi
 if [ "$LOGGING_F" = true ]; then mkdir -p logs; fi
 jq --version >/dev/null || abort "\`jq\` is not installed. install it with 'apt install jq' or equivalent"
@@ -41,9 +47,9 @@ for table_name in $(toml_get_table_names); do
 
 	if ((idx >= PARALLEL_JOBS)); then wait -n; else idx=$((idx + 1)); fi
 	declare -A app_args
-	excluded_patches=$(toml_get "$t" excluded-patches) || excluded_patches=""
-	included_patches=$(toml_get "$t" included-patches) || included_patches=""
-	exclusive_patches=$(toml_get "$t" exclusive-patches) || exclusive_patches=false
+	app_args[excluded_patches]=$(toml_get "$t" excluded-patches) || app_args[excluded_patches]=""
+	app_args[included_patches]=$(toml_get "$t" included-patches) || app_args[included_patches]=""
+	app_args[exclusive_patches]=$(toml_get "$t" exclusive-patches) || app_args[exclusive_patches]=false
 	app_args[version]=$(toml_get "$t" version) || app_args[version]="auto"
 	app_args[app_name]=$(toml_get "$t" app-name) || app_args[app_name]=$table_name
 	app_args[allow_alpha_version]=$(toml_get "$t" allow-alpha-version) || app_args[allow_alpha_version]=false
@@ -52,7 +58,6 @@ for table_name in $(toml_get_table_names); do
 			abort "ERROR: undefined build mode '${app_args[build_mode]}' for '${table_name}': only 'both', 'apk' or 'module' are allowed"
 		fi
 	} || app_args[build_mode]=apk
-	app_args[microg_patch]=$(toml_get "$t" microg-patch) || app_args[microg_patch]=""
 	app_args[uptodown_dlurl]=$(toml_get "$t" uptodown-dlurl) && {
 		app_args[uptodown_dlurl]=${app_args[uptodown_dlurl]%/}
 		app_args[uptodown_dlurl]=${app_args[uptodown_dlurl]%download}
@@ -74,16 +79,11 @@ for table_name in $(toml_get_table_names); do
 	app_args[module_prop_name]=$(toml_get "$t" module-prop-name) || {
 		app_name_l=${app_args[app_name],,}
 		if [ "${app_args[arch]}" = "all" ]; then
-			app_args[module_prop_name]="${app_name_l}-rv-jhc-magisk"
+			app_args[module_prop_name]="${app_name_l}-${RV_BRAND_F}-jhc"
 		else
-			app_args[module_prop_name]="${app_name_l}-${app_args[arch]}-rv-jhc-magisk"
+			app_args[module_prop_name]="${app_name_l}-${app_args[arch]}-${RV_BRAND_F}-jhc"
 		fi
 	}
-	app_args[patcher_args]="$(join_args "${excluded_patches}" -e) $(join_args "${included_patches}" -i)"
-	[ "$exclusive_patches" = true ] && app_args[patcher_args]+=" --exclusive"
-	if [ "${app_args[microg_patch]}" ] && [[ "${app_args[patcher_args]}" = *"${app_args[microg_patch]}"* ]]; then
-		abort "ERROR: Do not include microg in included or excluded patches list"
-	fi
 	if [ "$LOGGING_F" = true ]; then
 		logf=logs/"${table_name,,}.log"
 		: >"$logf"
